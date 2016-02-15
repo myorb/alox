fce86c6fb986eeebffe732d5dd27d6c934bc5aed
Alex Shalaiev <itdep24@gmail.com>
2016-02-15 22:51:39 +0200

    fix grid

--- views/apartment/grid.php
+++ views/apartment/grid.php
@@ -7,22 +7,32 @@ use kartik\helpers\Html;
  * Time: 20:38
  */
 ?>
-<div class="row">
-    <?php foreach($dataProvider->getModels() as $model): ?>
-        <div class="col-lg-3 col-md-4 col-xs-6 thumb">
-            <?=\yii\bootstrap\Html::a(
-                \yii\bootstrap\Html::img(
-                    $model->image_link?
-                        $model->image_link:
-                        'http://placehold.it/400x300',
-                    ['height'=>'200px','style'=>['max-height'=>'200px','min-height'=>'200px']]),
-                ['details','id'=>$model->id],
-                [
-                    'class'=>"thumbnail",
-                    'onclick'=>"$('#modal').modal('show').find('.modal-body').load($(this).attr('href'));return false",
-                ]
-            )
-            ?>
-        </div>
-    <?php endforeach ?>
+<div class="grid-ap-index">
+    <div class="row">
+        <?php foreach(array_chunk($dataProvider->getModels(),4) as $v): ?>
+            <?php foreach($v as $model): ?>
+                <div class="col-lg-3 col-md-4 col-xs-6 thumb portfolio-item">
+                    <?=\yii\bootstrap\Html::a(
+                        \yii\bootstrap\Html::img(
+                            $model->image_link?
+                                $model->image_link:
+                                'http://placehold.it/400x300',
+                            ['height'=>'200px','style'=>['max-height'=>'200px','min-height'=>'200px']]).'<span class="title">'.$model->title.'</span>',
+//                Html::a('<span class="glyphicon glyphicon-link"></span>',$model->url,['target'=>'_blank','title'=>'olx']),
+                        ['details','id'=>$model->id],
+                        [
+                            'class'=>"thumbnail",
+                            'onclick'=>"$('#modal').modal('show').find('.modal-body').load($(this).attr('href'));return false",
+                        ]
+                    )
+                    ?>
+                </div>
+            <?php endforeach ?>
+            <div class="clearfix"></div>
+        <?php endforeach ?>
+    </div>
 </div>
+    
+
+
+
